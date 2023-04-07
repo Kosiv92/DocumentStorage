68c9c2185bb1942b928f9263742aac353a56ba1e
kosiv92 <kosiv92@gmail.com>
2023-04-08 00:09:59 +0800

    Add project files.

---
+++ DocumentStorage/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using DocumentStorage.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentStorage.Controllers
+{
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            var viewModel = new LoginViewModel
+            {
+                ReturnUrl = returnUrl,
+            };
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
